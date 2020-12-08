230a005d823bbf30b1175a6bb127ccaa19c90a4f
ericw142 <66480663+ericw142@users.noreply.github.com>
2020-12-08 12:50:19 -0500

    initial implementation of nodemailer

--- routes/router.js
+++ routes/router.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const nodemailer = require("nodemailer");
 
 module.exports = function(app) {
     app.get("/", (req, res) => {
@@ -20,6 +21,23 @@ module.exports = function(app) {
     // Contact Form
     app.post("/sendcontactform", (req, res) => {
         console.log(req.body);
+        async function sendMail() {
+            let transporter = nodemailer.createTransport({
+                service: "gmail",
+                auth: {
+                  user: "procourse48@gmail.com",
+                  pass: "jH^Vx660nZcI"
+                },
+              });
+        
+              let info = await transporter.sendMail({
+                from: 'procourse48@gmail.com',
+                to: 'eric.h.warshawsky@gmail.com',
+                subject: "Someone contacted you from your Portfolio Site",
+                html: "<b>You have been contacted by "+req.body.email+"</b><p>First Name: "+req.body.firstName+" Last Name: "+req.body.lastName+".</p><br><p>"+req.body.message+"</p>",
+              });
+        }
+        sendMail().catch(console.error);
         res.sendFile(path.join(__dirname, "../public/index.html"));
     })
 }

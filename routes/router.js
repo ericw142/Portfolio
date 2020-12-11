1cad41e2378aee9ef0e4c115f154c214f4cbd968
ericw142 <66480663+ericw142@users.noreply.github.com>
2020-12-11 14:17:54 -0500

    updated nodemail, small updates

--- routes/router.js
+++ routes/router.js
@@ -25,13 +25,13 @@ module.exports = function(app) {
             let transporter = nodemailer.createTransport({
                 service: "gmail",
                 auth: {
-                  user: "procourse48@gmail.com",
-                  pass: "jH^Vx660nZcI"
+                  user: "ericw142portfolio@gmail.com",
+                  pass: "A@ZgXTX5wY^f"
                 },
               });
         
               let info = await transporter.sendMail({
-                from: 'procourse48@gmail.com',
+                from: 'ericw142portfolio@gmail.com',
                 to: 'eric.h.warshawsky@gmail.com',
                 subject: "Someone contacted you from your Portfolio Site",
                 html: "<b>You have been contacted by "+req.body.email+"</b><p>First Name: "+req.body.firstName+" Last Name: "+req.body.lastName+".</p><br><p>"+req.body.message+"</p>",

8c3fcc4f4134e27a84268a9c0bca564e536c77c8
ericw142 <66480663+ericw142@users.noreply.github.com>
2021-01-26 13:05:10 -0500

    updated link to stylesheet

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import '../src/assets/CSS/styles.css'
 
 
 ReactDOM.render(
